ca01ba9979909ba9d905d623c9689329756255c8
Domício Medeiros <domicioam@gmail.com>
2018-12-07 10:59:45 -0200

    Adding combined reducers to the store. Applying logger middleware to the store.

---
+++ src/app/components/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+import { User } from './User';
+import { Main } from './Main';
+
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+  changeUsername(newName) {
+
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Main changeUsername={this.changeUsername.bind(this)} />
+        <User username={this.props.user.name}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.userReducer,
+    math: state.mathReducer
+  };
+}; 
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setName: (name) => {
+      dispatch({
+        type: "SET_NAME",
+        payload: name
+      });
+    }
+  };
+}; 
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/app/index.js
+++ src/app/index.js
@@ -1,43 +1,15 @@
-// import React, { Component } from "react";
-// import { render } from "react-dom";
+import React from "react";
+import { render } from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-// import { User } from './components/User';
-// import { Main } from './components/Main';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import App from './components/App';
 
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       username: "Max"
-//     };
-//   }
-
-//   changeUsername(newName) {
-//     this.setState({
-//       username: newName
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="container">
-//         <Main changeUsername={this.changeUsername.bind(this)} />
-//         <User username={this.state.username} />
-//       </div>
-//     );
-//   }
-// }
-
-// render(<App />, window.document.getElementById('app'));
-
-import { createStore } from 'redux';
-
-const initialState = {
+const mathReducer = (state = {
   result: 1,
   lastValues: []
-};
-
-const reducer = (state = initialState, action) => {
+}, action) => {
   switch (action.type) {
     case "ADD":
       state = {
@@ -60,23 +32,50 @@ const reducer = (state = initialState, action) => {
   return state;
 };
 
-const store = createStore(reducer);
+const userReducer = (state = {
+  name: "Max",
+  age: 27
+}, action) => {
+  switch (action.type) {
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload
+      };
+
+      break;
+    case "SET_AGE":
+      state = {
+        ...state,
+        name: action.payload
+      };
+
+      break;
+  }
+
+  return state;
+};
+
+const myLogger = (store) => (next) => (action) => {
+  console.log("Logged Action: ", action);
+  next(action);
+}
+
+const store = createStore(
+  combineReducers({
+    mathReducer,
+    userReducer
+  }), {},
+  applyMiddleware(logger())
+);
 
 store.subscribe(() => {
-  console.log("Store updated!", store.getState());
+  //console.log("Store updated!", store.getState());
 });
 
-store.dispatch({
-  type: "ADD",
-  payload: 100
-});
-
-store.dispatch({
-  type: "ADD",
-  payload: 22
-});
-
-store.dispatch({
-  type: "SUBSTRACT",
-  payload: 80
-});
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  window.document.getElementById('app')
+);
